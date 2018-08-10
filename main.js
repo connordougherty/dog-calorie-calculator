6ba29b53cf776cc2e9438eb944618b3e546027a0
Connor Dougherty <connordougherty01@gmail.com>
2018-08-10 11:02:00 -0400

    Add option for daily energy needs

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let restingEnergyRequirements;
+let caloriesPerDay;
 
 
 // DOM Elements
 const form = document.querySelector('#form');
 const weightInput = document.querySelector('#weight-input');
+const dailyEnergyNeeds = document.querySelector('#daily-energy-needs')
 const caloricNeeds = document.querySelector('#caloric-needs');
 const pound = document.querySelector('#pound');
 const kilogram = document.querySelector('#kilogram');
@@ -14,9 +16,38 @@ function calculate() {
     event.preventDefault();
     
     if (pound.checked) {
-        caloricNeeds.innerHTML =  Math.round(Math.pow(weightInput.value * 0.45359237, .75) * 70);
+        restingEnergyRequirements =  Math.pow(weightInput.value * 0.45359237, .75) * 70;
     } else if (kilogram.checked) {
-        caloricNeeds.innerHTML =  Math.round(Math.pow(weightInput.value, .75) * 70);
+        restingEnergyRequirements =  Math.pow(weightInput.value, .75) * 70;
     }
+
+    switch (dailyEnergyNeeds.value) {
+        case 'Neutered adult':
+            caloriesPerDay = restingEnergyRequirements * 1.6
+            break;
+        case 'Intact adult':
+            caloriesPerDay = restingEnergyRequirements * 1.8
+            break;
+        case 'Inactive/obese prone':
+            caloriesPerDay = restingEnergyRequirements * 1.3
+            break;
+        case 'Weight loss':
+            caloriesPerDay = restingEnergyRequirements * 1.0
+            break;
+        case 'Weight gain':
+            caloriesPerDay = restingEnergyRequirements * 1.6
+            break;
+        case 'Active, working dogs':
+            caloriesPerDay = restingEnergyRequirements * 3.5
+            break;
+        case 'Puppy 0-4 months':
+            caloriesPerDay = restingEnergyRequirements * 3.0
+            break;
+        case 'Puppy 4 months to adult':
+            caloriesPerDay = restingEnergyRequirements * 2.0
+            break; 
+    }
+
+    caloricNeeds.innerHTML = Math.round(caloriesPerDay);
 }
 
